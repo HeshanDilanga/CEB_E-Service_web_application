efb43ffe2cf7ea59f61903ba3b52e41a8e2655a6
RashmiDilshara <rashmiabeywickrama20@gmail.com>
2023-06-20 22:08:12 +0530

    Database PHP

---
+++ PHP Files/server connection.php
@@ -0,0 +1,10 @@
+<?php
+  $dbServerName = "localhost";
+  $dbUserName   = "root";
+  $dbPassword   ="";
+  $dbName       ="ceb_e_bill_db";
+
+  $connect = mysqli_connect($dbServerName,$dbUserName,$dbPassword,$dbName);
+
+
+?>
